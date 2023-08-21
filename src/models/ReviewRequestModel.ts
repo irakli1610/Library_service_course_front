0fbfbc0e43228a39a92d503f0ffa3f1ff3419655
comp_theme <iraklitsulukidze14@gmail.com>
2023-08-21 23:55:54 +0400

    initial commit without admin services

---
+++ src/models/ReviewRequestModel.ts
@@ -0,0 +1,14 @@
+class ReviewRequestModel{
+    rating: number;
+    bookId: number;
+    reviewDescription?: string;
+
+    constructor(rating: number,
+                bookId: number,
+                reviewDescription?: string){
+        this.bookId=bookId;
+        this.rating=rating;
+        this.reviewDescription=reviewDescription;
+    }
+}
+export default ReviewRequestModel;
