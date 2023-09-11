9bc36c92e67b0f92a9ff376b3f96142dd3121d15
comp_theme <iraklitsulukidze14@gmail.com>
2023-09-11 16:14:26 +0400

    add AdminServices

---
+++ src/models/AdminMessageRequest.ts
@@ -0,0 +1,10 @@
+class AdfminMessageRequest{
+    id: number;
+    response: string
+
+    constructor(id: number, response: string){
+        this.id=id;
+        this.response=response;
+    }
+}
+export default AdfminMessageRequest;
